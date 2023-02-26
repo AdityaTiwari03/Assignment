6a9c33f442533c4e64d47e86277702d5acfc81f6
Shubham <sbdaule5@gmail.com>
2023-02-26 17:31:36 +0530

    Add q5 graph

--- q5.sh
+++ q5.sh
@@ -4,9 +4,8 @@ echo "Hi" > f1
 git add f1
 git commit -m "1st"
 echo "hello" > f2
-git graph
-git commit --amend -m "Changed"
-git add f3
 git commit --amend --no-edit
 git graph
-git rm f3
+sleep 1
+git commit --amend -m "Changed"
+git graph
