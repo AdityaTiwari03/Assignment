19113717283f4d66f8af150201055b0cffad8433
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2023-02-26 13:01:50 +0530

    corrected q10

--- q10.sh
+++ q10.sh
@@ -1,18 +1,30 @@
 #! /usr/bin/bash
 git init
+git remote add origin https://github.com/AdityaTiwari03/New2.git
+
+# first commit
 touch f1
 git add f1
-giit commit -m "1st"
+git commit -m "1st"  
+
+# second commit 
 echo "hhh" > f2
 git add f2
 git commit -m "2"
+
+# changed  first file, third commit
 echo "abc" > f1
 git add f1
 git commit -m "3"
-git checkout -b feature
+git push -u --force origin master
+
+git remote set-head origin master
+
+# creating new branch feature + commit on feature branch
+git checkout -b product
 echo "abcd" > f1
 git add f1
-git commit -m "4"git remote add origin (URL)
-git push -u origin 
-git checkout master
-git graph	
+git commit -m "4"
+
+git checkout HEAD~3
+git graph
